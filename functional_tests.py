2d229b1170f7f1cc46260e30c7220f4ac8b76a61
Luan Andrade <luansandrade92@hotmail.com>
2020-07-08 15:00:18 -0300

    Teste funcional verifica que podemos adicionar um novo to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
-import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -23,23 +25,53 @@ class NewVisitorTest(unittest.TestCase):
 		mencionam listas de tarefas (to-do)
 		"""
 		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+
+
+		"""
+		Ela é convidada a inserir um item de tarefa imediatamente
+		"""
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+			)
+
+		"""
+		Ela digita "Buy peacock feathers" (comprar penas de pavão)
+		em uma caixa de texto (o hobby de Edith é fazer iscas de 
+		pesca com fly)
+		"""
+		inputbox.send_keys('Buy peacock feathers')
+
+		
+		"""
+		Quando ela tecla enter, a página é atualizada, e agora
+		a página lista "i: Buy peacock feathers" como um item em uma
+		lista de tarefas
+		"""	
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+			)
+
+
+		"""
+		Ainda contina havendo uma caixa de texto convidando-a a 
+		acrescentar outro item. Ela insere "Use peacock to make a fly"
+		(Usar penas de pavão para fazer um fly - Edit é bem metódica)
+
+		"""
+
 		self.fail('Finish the test!')
 
 """
-Ela é convidada a inserir um item de tarefa imediatamente
 
-
-Ela digita "Buy peacock feathers" (comprar penas de pavão)
-em uma caixa de texto (o hobby de Edith é fazer iscas de 
-pesca com fly)
-
-Quando ela tecla enter, a página é atualizada, e agora
-a página lista "i: Buy peacock feathers" como um item em uma
-lista de tarefas
-
-Ainda contina havendo uma caixa de texto convidando-a a 
-acrescentar outro item. Ela insere "Use peacock to make a fly"
-(Usar penas de pavão para fazer um fly - Edit é bem metódica)
 
 A página é atualizada novamente e agora mostra os dois itens
 em sua lista
