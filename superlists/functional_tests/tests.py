cef2443cd1d4bac949600fbede2a679422e43b1d
Luan Andrade <luansandrade92@hotmail.com>
2020-07-12 21:28:03 -0300

    Remove waits explicitos

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -6,6 +6,8 @@ from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
+MAX_WAIT = 10
+
 class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
@@ -14,11 +16,22 @@ class NewVisitorTest(LiveServerTestCase):
 	def tearDown(self):
 		self.browser.quit()
 
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
 
+		while True:
+			try:
+				table = self.browser.find_element_by_id('id_list_table')
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(row_text, [row.text for row in rows])
+				return
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
+					
+			
+		
 	def test_can_start_a_list_and_retrieve_it_later(self):
 
 		"""
@@ -60,9 +73,7 @@ class NewVisitorTest(LiveServerTestCase):
 		lista de tarefas
 		"""	
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
 		"""
 		Ainda contina havendo uma caixa de texto convidando-a a 
@@ -72,14 +83,13 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-
+		
 		"""
 		A página é atualizada novamente e agora mostra os dois itens
 		em sua lista
 		"""
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
-		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
+		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		self.fail('Finish the test!')
 
