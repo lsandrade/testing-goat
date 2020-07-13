fe4ba8a4dcde160bbcdca4226ebf99d6995f5033
Luan Andrade <luansandrade92@hotmail.com>
2020-07-12 21:14:38 -0300

    Transforma testes funcionais em um app usando LiveServerTestCase

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,9 +1,12 @@
+from django.test import LiveServerTestCase
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
 import time
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -22,7 +25,7 @@ class NewVisitorTest(unittest.TestCase):
 		Edir ouviu falar de uma nova aplicação online interessante
 		para lista de tarefas. Ela decide verificar sua homepage
 		"""
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
 
 		"""
@@ -92,5 +95,3 @@ Ela acessa esse URL - sua lista de tarefas continua lá
 
 Satisfeita, ela volta a dormir
 """
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
