d9159cac0b0ce12c891a2144b4f7f6556931e778
Luan Andrade <luansandrade92@hotmail.com>
2020-07-16 14:56:49 -0300

    Primeiros passos criando testes funcionais para layout + estilo

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -86,10 +86,7 @@ class NewVisitorTest(LiveServerTestCase):
 		"""
 		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
-
 		# satisfeita ela vai dormir
-
-		# self.fail('Finish the test!')
 
 	def test_multiple_users_can_start_lists_at_different_urls(self):
 		# Edith starts a new to-do list
@@ -135,3 +132,28 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertIn('Buy milk', page_text)
 
 		# Satisfied, they both go back to sleep
+
+	def test_layout_and_styling(self):
+		# Edith goes to the home page
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		# She notices the input box is nicely centred
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=10
+		)
+
+		# She starts a new list and sees the input is nicely
+		# centred there too
+		inputbox.send_keys('testing')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: testing')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=10
+		)
